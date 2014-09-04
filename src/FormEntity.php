54c17ee57f25a766511638442714fc3daf862500
Andy Truong <thehongtt@gmail.com>
2014-09-04 15:59:04 +0700

    Add form.uid column.

--- src/FormEntity.php
+++ src/FormEntity.php
@@ -8,4 +8,10 @@ class FormEntity extends Entity
 {
 
     use \GO1\FormCenter\Entity\Type\EntityTypeTrait;
+
+    /**
+     * @var integer The user id of the profile owner.
+     */
+    public $uid;
+
 }
