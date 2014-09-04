eaf3083a32dc3fa693a7920542e3be8bbb9bf8d9
Andy Truong <thehongtt@gmail.com>
2014-09-04 09:25:23 +0700

    Dummy storage handler.

---
+++ src/DrupalEntityStorageHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\form_builder;
+
+use GO1\FormCenter\Entity\EntityInterface;
+use GO1\FormCenter\Entity\Storage\EntityStorageHandlerBase;
+
+class DrupalEntityStorageHandler extends EntityStorageHandlerBase
+{
+
+    /** @var string */
+    protected $name = 'drupal';
+
+    /** @var string */
+    protected $humanName = 'Drupal storage handler';
+
+    public function create(EntityInterface $entity)
+    {
+
+    }
+
+    public function delete(EntityInterface $entity)
+    {
+
+    }
+
+    public function deleteById($entityTypeName, $id)
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param EntityInterface $entity
+     * @param array $cmds
+     */
+    public function patch(EntityInterface $entity, array $cmds = array())
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param EntityInterface $entity
+     */
+    public function update(EntityInterface $entity)
+    {
+
+    }
+
+}
