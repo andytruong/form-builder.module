fa05943ef6d7fb44993a9d9bb3f89ef4e4b8f7aa
Andy Truong <thehongtt@gmail.com>
2014-09-04 16:14:32 +0700

    Oh use wrong trait.

--- src/FormEntity.php
+++ src/FormEntity.php
@@ -7,7 +7,7 @@ use Entity;
 class FormEntity extends Entity
 {
 
-    use \GO1\FormCenter\Entity\Type\EntityTypeTrait;
+    use \GO1\FormCenter\Form\FormTrait;
 
     /**
      * @var integer The user id of the profile owner.
