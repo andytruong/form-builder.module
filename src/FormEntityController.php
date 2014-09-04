9772527d51357d62c32343bfa1a57e007a3e9169
Andy Truong <thehongtt@gmail.com>
2014-09-04 17:13:11 +0700

    Fix stupid autoloading issue.

--- src/FormEntityController.php
+++ src/FormEntityController.php
@@ -20,4 +20,21 @@ class FormEntityController extends EntityAPIController
         return $entities;
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function query($ids, $conditions, $revision_id = FALSE)
+    {
+        // I hate but I do not know better way to fix this:
+        // At /admin/structure/fob-form/manage/%id, Drupal load entity before
+        // executing hook_init, where composer_manager module registers composer's
+        // autoloader.
+        $fns = spl_autoload_functions();
+        if (is_string($fns[0])) {
+            composer_manager_init();
+        }
+
+        return parent::query($ids, $conditions, $revision_id);
+    }
+
 }
