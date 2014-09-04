5ccc97c5f90e42a7f88ba4be33a144070cc4b9eb
Andy Truong <thehongtt@gmail.com>
2014-09-04 17:57:03 +0700

    Stash!

--- src/FormEntityController.php
+++ src/FormEntityController.php
@@ -3,21 +3,54 @@
 namespace Drupal\form_builder;
 
 use EntityAPIController;
+use GO1\FormCenter\Manager\Manager;
 
 class FormEntityController extends EntityAPIController
 {
+
+    /** @var Manager */
+    private $manager;
+
+    public function getManager()
+    {
+        if (null === $this->manager) {
+            $this->manager = form_builder_manager();
+        }
+        return $this->manager;
+    }
 
     public function load($ids = array(), $conditions = array())
     {
         $entities = parent::load($ids, $conditions);
         foreach ($entities as &$entity) {
-            foreach (array('entity_types', 'form_fields', 'layout_options', 'listeners') as $key) {
-                if (!empty($entity->{$key})) {
-                    $entity->{$key} = json_decode($entity->{$key}, true);
-                }
-            }
+            $this->fixEntity($entity);
         }
         return $entities;
+    }
+
+    private function fixEntity(FormEntity $entity)
+    {
+        /* @var $entity FormEntity */
+        foreach (array('entity_types', 'form_fields', 'layout_options', 'listeners') as $key) {
+            if (!empty($entity->{$key})) {
+                $entity->{$key} = json_decode($entity->{$key}, true);
+            }
+        }
+
+        // Fix entity types
+        if (!empty($entity->entity_types)) {
+            foreach ($entity->entity_types as $entity_type) {
+                kpr($entity_type);
+                exit;
+            }
+        }
+
+        // fix form fields
+        if (!empty($entity->form_fields)) {
+            foreach ($entity->form_fields as $fieldUuid => $fieldName) {
+                // $field = $this->getManager()->getFieldType($name);
+            }
+        }
     }
 
     /**
