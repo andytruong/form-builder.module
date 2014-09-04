71f64781776a955e5f547bbbf7710bcfe399df36
Andy Truong <thehongtt@gmail.com>
2014-09-04 09:25:51 +0700

    Add EntityTypeProvider.

---
+++ src/EntityTypeProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\form_builder;
+
+use GO1\FormCenter\Entity\Type\EntityTypeInterface;
+use GO1\FormCenter\Extension\GOC\EntityTypeProvider;
+
+class EntityTypeProvider extends EntityTypeProvider
+{
+
+    public function __construct()
+    {
+        $this->setName('drupal.provider.entity_type');
+        $this->setHumanName('[Drupal] Entity type provider');
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return EntityTypeInterface[]
+     */
+    public function discoverEntityTypes()
+    {
+        $entityTypes = [];
+        foreach (entity_get_info() as $entityName => $entityInfo) {
+            $entityType = new DrupalEntityType();
+            $entityType->setName($entityName);
+            $entityType->setHumanName($entityInfo['label']);
+            $entityType->setIDKey($entityInfo['entity keys']['id']);
+            $entityTypes[$entityName] = $entityType;
+        }
+        return $entityTypes;
+    }
+
+}
