da7f9b5266f9839f4f79105cccfdc9ce2ef559a9
Andy Truong <thehongtt@gmail.com>
2014-09-05 14:27:49 +0700

    Remove classic Drupal form in favor of AngularJS app.

--- src/Controller/EntityEditController.php
+++ src/Controller/EntityEditController.php
@@ -16,17 +16,24 @@ class EntityEditController
         angularjs_init_application('FormBuilderEntityEditingForm');
         drupal_add_js(drupal_get_path('module', 'form_builder') . '/js/entity.editing.js');
         drupal_add_js(array('FormBuilder' => array(
-                'available' => array(
+                'available'   => array(
+                    'languages' => array_map(function($language) {
+                            return $language->name;
+                        }, language_list('enabled')[1]),
                     'entityTypes' => array_map(
                         function($type) {
                             return $type->getHumanName();
                         }, form_builder_manager()->getEntityTypes()
                     ),
-                    'fields'    => array_map(function($field) {
+                    'fields'      => array_map(function($field) {
                             return $field->getHumanName();
                         }, form_builder_manager()->getFields()),
                 ),
-                'entity' => $this->entity,
+                'entity' => array(
+                    'title'       => $this->entity->getTitle(),
+                    'status'      => $this->entity->getStatus(),
+                    'entityTypes' => $this->entity->getEntityTypes(),
+                ),
             )), array('type' => 'setting'));
     }
 
