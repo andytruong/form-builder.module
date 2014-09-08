f5ac3a7e377a15ba4197fa7f11a5ab7adda26c23
Andy Truong <thehongtt@gmail.com>
2014-09-08 15:02:43 +0700

    Move render methods to sub class.

--- src/Controller/EntityEditController.php
+++ src/Controller/EntityEditController.php
@@ -2,48 +2,23 @@
 
 namespace Drupal\form_builder\Controller;
 
-use AndyTruong\Serializer\Serializer;
+use Drupal\form_builder\Controller\EntityEditController\Render;
+use Drupal\form_builder\Controller\EntityEditController\SubmitHandler;
 use Drupal\form_builder\FormEntity;
-use GO1\FormCenter\Field\FieldInterface;
 
 class EntityEditController
 {
 
-    private $entity;
-    private $template;
+    public $entity;
 
     public function __construct(FormEntity $entity)
     {
         $this->entity = $entity;
-        $this->template = drupal_get_path('module', 'form_builder') . '/templates/form.edit.tpl.php';
     }
 
-    public static function pageCallback($entityType, $entity)
+    public function getRender()
     {
-        $me = new self($entity);
-        return $me->render($entityType);
-    }
-
-    public function render()
-    {
-        $js = array();
-        $js[0] = array('data' => '//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.0/angular.min.js', array('external' => true));
-        $js[1] = drupal_get_path('module', 'form_builder') . '/js/entity.editing.js';
-        $js[2] = array(
-            'type' => 'setting',
-            'data' => array('FormBuilder' => $this->getRenderInfo()),
-        );
-
-        return array(
-            // '#prefix'   => kpr($js[1]['data']['FormBuilder'], true),
-            '#markup'   => theme_render_template(
-                $this->template, array(
-                'data' => $js[2]['data']['FormBuilder']
-            )),
-            '#attached' => array(
-                'js' => $js
-            )
-        );
+        return new Render($this);
     }
 
     private function getRenderInfo()
@@ -54,31 +29,17 @@ class EntityEditController
         );
     }
 
-    private function getAvailableInfo()
+    public static function pageCallback($entityType, $entity)
     {
-        $allEntityTypes = array_map(function($type) {
-            return (new Serializer())->toArray($type);
-        }, form_builder_manager()->getEntityTypes());
+        $me = new self($entity);
 
-        $addedEntityTypeNames = array_map(function($type) {
-            return $type->getName();
-        }, $this->entity->getEntityTypes());
+        switch ($_SERVER['REQUEST_METHOD']) {
+            case 'GET':
+                return $me->getRender()->render($entityType);
 
-        $fields = array_map(function(FieldInterface $field) {
-            return (new Serializer())->toArray($field);
-        }, form_builder_manager()->getFields($addedEntityTypeNames));
-
-        return array(
-            'languages'   => language_list('enabled')[1],
-            'entityTypes' => $allEntityTypes,
-            'fields'      => $fields,
-        );
-    }
-
-    private function getEntityInfo()
-    {
-        $info = $this->entity->toArray();
-        return $info;
+            case 'POST':
+                return $me->getSubmitHandler()->handle(json_decode(file_get_contents('php://input'), true));
+        }
     }
 
 }
---
+++ src/Controller/EntityEditController/Render.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\form_builder\Controller\EntityEditController;
+
+use AndyTruong\Serializer\Serializer;
+use Drupal\form_builder\Controller\EntityEditController;
+use GO1\FormCenter\Field\FieldInterface;
+
+class Render
+{
+
+    /** @var EntityEditController */
+    private $ctrl;
+
+    /** @var string */
+    public $template;
+
+    public function __construct($ctrl)
+    {
+        $this->ctrl = $ctrl;
+        $this->template = drupal_get_path('module', 'form_builder') . '/templates/form.edit.tpl.php';
+    }
+
+    public function render()
+    {
+        $js = array();
+        $js[0] = array('data' => '//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.0/angular.min.js', array('external' => true));
+        $js[1] = drupal_get_path('module', 'form_builder') . '/js/entity.editing.js';
+        $js[2] = array(
+            'type' => 'setting',
+            'data' => array('FormBuilder' => $this->getRenderInfo()),
+        );
+
+        return array(
+            '#prefix'   => !empty($_GET['debug']) ? kpr($js[1]['data']['FormBuilder'], true) : '',
+            '#markup'   => theme_render_template(
+                $this->template, array(
+                'data' => $js[2]['data']['FormBuilder']
+            )),
+            '#attached' => array('js' => $js)
+        );
+    }
+
+    private function getRenderInfo()
+    {
+        return array(
+            'available' => $this->getAvailableInfo(),
+            'entity'    => $this->getEntityInfo(),
+        );
+    }
+
+    private function getAvailableInfo()
+    {
+        $allEntityTypes = array_map(function($type) {
+            return (new Serializer())->toArray($type);
+        }, form_builder_manager()->getEntityTypes());
+
+        $addedEntityTypeNames = array_map(function($type) {
+            return $type->getName();
+        }, $this->ctrl->entity->getEntityTypes());
+
+        $fields = array_map(function(FieldInterface $field) {
+            return (new Serializer())->toArray($field);
+        }, form_builder_manager()->getFields($addedEntityTypeNames));
+
+        return array(
+            'languages'   => language_list('enabled')[1],
+            'entityTypes' => $allEntityTypes,
+            'fields'      => $fields,
+        );
+    }
+
+    private function getEntityInfo()
+    {
+        $info = $this->ctrl->entity->toArray();
+        return $info;
+    }
+
+}
