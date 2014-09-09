ceb6613bdfc00c9d05a1f9d405d8f1a39421fcf6
Andy Truong <thehongtt@gmail.com>
2014-09-09 09:57:20 +0700

    More details for submit handler

--- src/Controller/EntityEditController/SubmitHandler.php
+++ src/Controller/EntityEditController/SubmitHandler.php
@@ -13,9 +13,48 @@ class SubmitHandler
         $this->ctrl = $ctrl;
     }
 
+    /**
+     * Main endpoint to handle user's submission.
+     *
+     * @param array $request
+     * @return array
+     */
     public function handle(array $request)
     {
+        if (!isset($request['action'])) {
+            $methods = at_camelize($request['action']);
+            unset($request['action']);
+            return $this->{$methods}($request);
+        }
 
+        if (!isset($request['action'])) {
+            return array('status' => 'FAIL', 'error' => 'Missing action');
+        }
+    }
+
+    /**
+     * When user add/remove entity types.
+     */
+    protected function handleUpdateEntityTypes()
+    {
+        return [
+            'status' => 'OK',
+            'fields' => [
+                'adding'   => [],
+                'removing' => [],
+            ],
+        ];
+    }
+
+    /**
+     * Save form-entity.
+     *
+     * @param array $request
+     * @return type
+     */
+    protected function handleSave(array $request)
+    {
+        return ['status' => 'OK', 'message' => 'Working…'];
     }
 
 }
