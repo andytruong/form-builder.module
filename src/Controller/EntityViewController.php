6ce65ae152cc36bfafe13d1a7b8a289578b67b3a
Andy Truong <thehongtt@gmail.com>
2014-09-17 13:48:48 +0700

    Fixing page request.

--- src/Controller/EntityViewController.php
+++ src/Controller/EntityViewController.php
@@ -8,15 +8,20 @@ use Drupal\form_builder\FormEntity;
 class EntityViewController
 {
 
-    public static function pageCallback(FormEntity $form)
+    public static function pageCallback(FormEntity $form, $slug = '', $pageNumber = 1)
     {
-        if (!$page = filter_input(INPUT_POST, 'form-page')) {
-            $page = 'master';
+        if ('POST' === $_SERVER['REQUEST_METHOD']) {
+            if (filter_input(INPUT_POST, 'form-page')) {
+                $pageNumber = filter_input(INPUT_POST, 'form-page');
+            }
+            return (new Submit($form, $pageNumber))->handle($_POST);
         }
 
-        return 'POST' === $_SERVER['REQUEST_METHOD'] // check request method
-            ? (new Submit($form, $page))->handle($_POST) // submit
-            : $form->render($page); // render form
+        if ($slug !== $form->getSlug()) {
+            drupal_goto($form->getPath($pageNumber));
+        }
+
+        return $form->render($pageNumber);
     }
 
 }
