da3a2e8dfa4748615276686a7e85a6889d8aa1db
Andy Truong <thehongtt@gmail.com>
2014-09-24 11:20:16 +0700

    Fix FOB-97: Form listing > Add link to go to form page.

--- src/Controller/FormUIController.php
+++ src/Controller/FormUIController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\form_builder\Controller;
 
+use Drupal\form_builder\FormEntity;
 use EntityDefaultUIController;
 
 class FormUIController extends EntityDefaultUIController
@@ -21,6 +22,20 @@ class FormUIController extends EntityDefaultUIController
         $items['admin/structure/fob-form/manage/%entity_object']['page callback'] = 'Drupal\form_builder\Controller\EntityEditController::pageCallback';
 
         return $items;
+    }
+
+    /**
+     * @param array $conditions
+     * @param int $id
+     * @param FormEntity $entity
+     * @param array $additional_cols
+     * @return type
+     */
+    protected function overviewTableRow($conditions, $id, $entity, $additional_cols = array())
+    {
+        $return = parent::overviewTableRow($conditions, $id, $entity, $additional_cols);
+        $return[] = l(t('View'), $entity->getPath(1));
+        return $return;
     }
 
 }
