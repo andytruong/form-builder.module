8a545132b88770c56a3e7b82520a4cd5d9756526
Andy Truong <thehongtt@gmail.com>
2014-09-24 16:54:19 +0700

    Rename groups template file.

--- templates/form.edit.groups.tpl.php
+++ templates/form.edit.groups.tpl.php
@@ -10,16 +10,4 @@
     <button ng-click="pageNew()">
         Add<span ng-if="newPageAdding">ing…</span>
     </button>
-
-    <input type="text"
-           id="edit-new-group-title"
-           name="title"
-           size="60"
-           maxlength="255"
-           class="form-text"
-           placeholder="Group name…"
-           ng-model="newGroupTitle" />
-    <button ng-click="pageGroup()">
-        Add<span ng-if="newGroupAdding">ing…</span>
-    </button>
 </div>
