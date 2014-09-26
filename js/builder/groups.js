125626a8b30e76a244111707cb728c29e1acb8fc
Andy Truong <thehongtt@gmail.com>
2014-09-26 15:52:26 +0700

    Fix bug: Wrong weight on drag new field to group.

--- js/builder/groups.js
+++ js/builder/groups.js
@@ -6,9 +6,27 @@
         helper.groupIsEmpty = function (pageId, groupId) {
             for (var fieldId in this.entity.layoutOptions.pages[pageId].fields)
                 if (typeof this.entity.layoutOptions.pages[pageId].fields[fieldId].parent !== 'undefined')
-                    if (this.entity.layoutOptions.pages[pageId].fields[fieldId].parent === groupId)
+                    if (groupId === this.entity.layoutOptions.pages[pageId].fields[fieldId].parent)
                         return false;
             return true;
+        };
+
+        helper.groupFixFieldWeight = function (pageId, groupId) {
+            var arr = [];
+
+            for (var fieldId in this.entity.layoutOptions.pages[pageId].fields)
+                if (typeof this.entity.layoutOptions.pages[pageId].fields[fieldId].parent !== 'undefined')
+                    if (groupId === this.entity.layoutOptions.pages[pageId].fields[fieldId].parent)
+                        arr.push({id: fieldId, weight: this.entity.layoutOptions.pages[pageId].fields[fieldId].weight});
+
+            arr.sort(function (a, b) {
+                return a.weight - b.weight;
+            });
+
+            for (var i in arr) {
+                var fieldId = arr[i].id;
+                this.entity.layoutOptions.pages[pageId].fields[fieldId].weight = 2 * i;
+            }
         };
 
         helper.groupNewFieldset = function () {
@@ -21,6 +39,8 @@
          * 3. Drag group to group
          */
         helper.groupFieldOnDrop = function ($channel, $data, toPageId, groupId, toFieldId) {
+            this.groupFixFieldWeight(toPageId, groupId);
+
             switch ($channel) {
                 case 'fieldInRoot':
                 case 'fieldInGroup':
