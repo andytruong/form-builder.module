ac76776f518d5bbc8346a89204418dca8ca0bb6a
Andy Truong <thehongtt@gmail.com>
2014-09-29 11:58:42 +0700

    Add before and after dropzone.

--- templates/form.edit/pages/stack.tpl.php
+++ templates/form.edit/pages/stack.tpl.php
@@ -8,16 +8,26 @@
     No field.
   </li>
 
-          ng-attr-ui-draggable="{{itemInfo.isGroup ? false : true}}"
-          ng-attr-drag-channel="{{itemInfo.isGroup ? '' : 'fieldInRoot'}}"
-          drag='{ "itemInfo": {{itemInfo}} }'
-          drop-channel="*"
-          drop-validate="fieldDragValidate($channel, $data)"
-          ui-on-Drop="fieldOnDrop($channel, $data, itemInfo.uuid, pageUuid)">
-    <div ng-if="itemInfo.isGroup"><?php include 'stack/group.tpl.php'; ?></div>
-    <div ng-if="!itemInfo.isGroup"><?php include 'stack/field.tpl.php'; ?></div>
   <li ng-attr-class="{{itemInfo.isGroup ? 'draggable group field' : 'draggable field'}}"
       ng-show="!item.parent"
+      ng-repeat="itemInfo in pageStack[pageUuid]|orderBy:'weight'">
+
+    <div class="dropzone"
+         drop-channel="*"
+         drop-validate="fieldDragValidate($channel, $data)"
+         ui-on-Drop="fieldOnDrop($channel, $data, itemInfo.uuid, pageUuid)"></div>
+
+    <div ng-attr-ui-draggable="{{itemInfo.isGroup ? false : true}}"
+         ng-attr-drag-channel="{{itemInfo.isGroup ? '' : 'fieldInRoot'}}"
+         drag='{ "itemInfo": {{itemInfo}} }'>
+      <div ng-if="itemInfo.isGroup"><?php include 'stack/group.tpl.php'; ?></div>
+      <div ng-if="!itemInfo.isGroup"><?php include 'stack/field.tpl.php'; ?></div>
+    </div>
+
+    <div class="dropzone"
+         drop-channel="*"
+         drop-validate="fieldDragValidate($channel, $data)"
+         ui-on-Drop="fieldOnDrop($channel, $data, itemInfo.uuid, pageUuid)"></div>
   </li>
 
   <!-- User drags new field to page, ask server for things… -->
