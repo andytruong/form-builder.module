a295172a0b0cb4c96d39b42dc8222c20140998d8
Andy Truong <thehongtt@gmail.com>
2014-09-29 14:41:01 +0700

    Fix FOB-107: Form edit > Fieldset options are not saved.

--- templates/form.edit/pages/stack/group.tpl.php
+++ templates/form.edit/pages/stack/group.tpl.php
@@ -20,11 +20,17 @@
   <!-- Config for fieldset -->
   <div class="group-options" ng-if="'fieldset' === itemInfo.type">
     <div class="form-item form-type-checkbox form-item-collapsible">
-      <input type="checkbox" id="edit-collapsible-{{itemInfo.uuid}}" class="form-checkbox" />
+      <input type="checkbox"
+             id="edit-collapsible-{{itemInfo.uuid}}"
+             class="form-checkbox"
+             ng-model="entity.layoutOptions.pages[pageId].groups[itemInfo.uuid].options.collapsible" />
       <label class="option" for="edit-collapsible-{{itemInfo.uuid}}">Collapsible</label>
     </div>
     <div class="form-item form-type-checkbox form-item-collapsed">
-      <input type="checkbox" id="edit-collapsed-{{itemInfo.uuid}}" class="form-checkbox" />
+      <input type="checkbox"
+             id="edit-collapsed-{{itemInfo.uuid}}"
+             class="form-checkbox"
+             ng-model="entity.layoutOptions.pages[pageId].groups[itemInfo.uuid].options.collapsed" />
       <label class="option" for="edit-collapsed-{{itemInfo.uuid}}">Collapsed</label>
     </div>
   </div>
