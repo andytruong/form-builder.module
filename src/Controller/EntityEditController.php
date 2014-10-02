c7275b3afe259e37b21e9a89a08310376f6fb520
Andy Truong <thehongtt@gmail.com>
2014-10-02 07:34:37 +0700

    Remove resolved @todo items.

--- src/Controller/EntityEditController.php
+++ src/Controller/EntityEditController.php
@@ -6,16 +6,6 @@ use Drupal\form_builder\Controller\EntityEditController\Render;
 use Drupal\form_builder\Controller\EntityEditController\SubmitHandler;
 use Drupal\form_builder\FormEntity;
 
-/**
- * TODO:
- *
- * 1. User add/remove entity type => update fields.
- * 2. Available fields list
- * 3. Added fields list
- * 4. …
- * n. Submit handler
- *
- */
 class EntityEditController
 {
 
